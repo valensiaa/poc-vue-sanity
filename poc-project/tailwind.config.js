0788d5cddc5756c91dba060a8e9d731c1871c106
valensiaa28 <Valentyna@stradigi.eu>
2024-06-12 10:58:17 +0200

    add public repo

--- poc-project/tailwind.config.js
+++ poc-project/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx, html}",
   ],
   theme: {
     extend: {
